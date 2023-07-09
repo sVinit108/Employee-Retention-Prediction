fb7ea5f7f00b944ce7714c39c576a08fd8b2c7c7
Vinit <sharmavinit.1008@gmail.com>
2023-07-09 12:55:10 +0530

    data ingestion added

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+import pandas as pd
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+from src.logger import logging
+from src.exception import CustomException
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join('artifacts','test.csv')
+    test_data_path = os.path.join('artifacts','test.csv')
+    raw_data_path = os.path.join('artifacts','raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info('Entered the data ingestion method')
+
+        try:
+            df = pd.read_csv(r"Data/data.csv")
+            logging.info("Read the data as dataframe")
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info('Trian test split initialized')
+            train_set, test_set = train_test_split(df,test_size=0.3,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of the data is completed")
+
+            return(
+            self.ingestion_config.train_data_path,
+            self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data,test_data = obj.initiate_data_ingestion()
