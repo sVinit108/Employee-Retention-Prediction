880396a1174b442b21b00db50405b9c43b4800cc
Vinit <sharmavinit.1008@gmail.com>
2023-07-17 14:16:01 +0530

    "#"

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,8 +10,9 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import LabelEncoder,StandardScaler
 
 from src.logger import logging
-from src.exception import CustomException
+from src.utils import save_object
 from src.utils import num_cat_split
+from src.exception import CustomException
 
 @dataclass
 class DataTransformationConfig:
@@ -21,7 +22,7 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
     
-    def data_transformer(self):
+    def data_transformer_obj_creation(self):
         '''This function performs data transformation'''
         try:
             # Numerical-Categorical features seperation
@@ -54,4 +55,35 @@ class DataTransformation:
             return preprocessor
     
         except Exception as e:
-            CustomException(e,sys)
+            raise CustomException(e,sys)
+    
+    def data_transformation(self,train_path,test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            preprocessing_obj=self.data_transformer_obj_creation()
+
+            target_col = 'left'
+
+            train_df_input_feature=train_df.drop(columns=[target_col],axis=1)
+            train_df_output_feature=train_df[target_col]
+
+            test_df_input_feature=test_df.drop(columns=[target_col],axis=1)
+            test_df_output_feature=test_df[target_col]
+
+            logging.info("Applying preprocessing object on training dataframe and testing dataframe.")
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(train_df_input_feature)
+            input_feature_test_arr=preprocessing_obj.transform(test_df_input_feature)
+
+            train_arr=np.c_[input_feature_train_arr,np.array(train_df_output_feature)]
+            test_arr=np.c_[input_feature_test_arr,np.array(test_df_output_feature)]
+
+            logging.info(f"Saved preprocessing object.")
+
+
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import dill
+from src.exception import CustomException
 
 def num_cat_split(df):
     isnull_sum = df.isnull().sum()
@@ -9,3 +13,14 @@ def num_cat_split(df):
     cat_vars_miss = [var for var in cat_vars if isnull_sum[var]>0]
 
     return num_vars,cat_vars,num_vars_miss,cat_vars_miss
+
+def save_object(obj,path):
+    try:
+        file_path = os.path.dirname(path)
+        os.makedirs(file_path,exist_ok=True)
+
+        with open(file_path,'wb') as f:
+            dill.dump(obj,f)
+            
+    except Exception as e:
+        raise CustomException(e,sys)
